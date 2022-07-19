e0afdf87629eae0c56f61846a0428945810dbf4e
Maria Paula Campos <maria.campos@ambevtech.com.br>
2022-07-19 14:19:38 -0300

    (Feat) testing modern react scanner

--- src/QrScan/index.js
+++ src/QrScan/index.js
@@ -1,28 +1,42 @@
 import { useState } from "react";
 import "../App.css";
-import QrReader from "react-qr-scanner";
+// import QrReader from "react-qr-scanner";
+import QrReader from "modern-react-qr-reader";
 import { Link } from "react-router-dom";
 
 function QrScan() {
   const [data, setData] = useState("No result");
+  const [scanner, setScanner] = useState(true);
   const [error, setError] = useState();
 
   const previewStyle = {
-    height: 340,
     width: "100%",
   };
 
+  if (error) {
+    return <div>Sorry... somenthing went wrong</div>;
+  }
+
   return (
     <div className="qrcode">
-      <QrReader
-        // delay={100}
-        // legacyMode
-        facingMode="rear"
-        style={previewStyle}
-        onError={(err) => console.error(err)}
-        onScan={(data) => console.log(data)}
-      />
-      <p>{data}</p>
+      {scanner && (
+        <QrReader
+          delay={300}
+          facingMode={"environment"}
+          onError={(err) => {
+            setError(err);
+            console.error(err);
+          }}
+          onScan={(data) => {
+            if (data) {
+              setData(data);
+              setScanner(false);
+            }
+          }}
+          style={previewStyle}
+        />
+      )}
+      <p>{data.text}</p>
       <Link to="/">Voltar</Link>
     </div>
   );
